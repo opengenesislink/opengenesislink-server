d4c22e77530bbd41a1faece3b049e78487a2a98c
OpenGenesis (MPL 2.0) <153564363+opengenesislink@users.noreply.github.com>
2026-09-18 11:31:37 +0200

    Wire Hypergrid Friends adapter into gateway

--- include/opengenesis/compat/hypergrid/server.hpp
+++ include/opengenesis/compat/hypergrid/server.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "opengenesis/compat/hypergrid/home_verifier.hpp"
+#include "opengenesis/compat/hypergrid/friends_adapter.hpp"
 #include "opengenesis/compat/hypergrid/service.hpp"
 #include "opengenesis/compat/hypergrid/session_store.hpp"
 #include "opengenesis/platform/socket.hpp"
@@ -19,7 +20,8 @@ public:
                     std::uint16_t port,
                     std::shared_ptr<HypergridService> service,
                     std::shared_ptr<HypergridSessionStore> sessions,
-                    std::shared_ptr<IHypergridHomeVerifier> verifier);
+                    std::shared_ptr<IHypergridHomeVerifier> verifier,
+                    std::shared_ptr<HypergridFriendsAdapter> friends);
     ~HypergridServer();
 
     HypergridServer(const HypergridServer&) = delete;
@@ -36,6 +38,7 @@ private:
     std::shared_ptr<HypergridService> service_;
     std::shared_ptr<HypergridSessionStore> sessions_;
     std::shared_ptr<IHypergridHomeVerifier> verifier_;
+    std::shared_ptr<HypergridFriendsAdapter> friends_;
     std::atomic_bool running_{false};
     std::thread thread_;
     platform::SocketHandle listen_fd_{platform::kInvalidSocket};
