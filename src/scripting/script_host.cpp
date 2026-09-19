63304c320c42f76f29176581ad0574f8755497fb
OpenGenesis (MPL 2.0) <153564363+opengenesislink@users.noreply.github.com>
2026-09-19 03:31:02 +0200

    Route Script World queries through ScriptHost

--- src/scripting/script_host.cpp
+++ src/scripting/script_host.cpp
@@ -35,6 +35,10 @@ std::optional<ScriptWorldActionType> world_action_type(const ScriptActionType ty
         case ScriptActionType::world_chat_say: return ScriptWorldActionType::chat_say;
         case ScriptActionType::world_chat_whisper: return ScriptWorldActionType::chat_whisper;
         case ScriptActionType::world_chat_shout: return ScriptWorldActionType::chat_shout;
+        case ScriptActionType::world_query_object: return ScriptWorldActionType::query_object;
+        case ScriptActionType::world_query_region: return ScriptWorldActionType::query_region;
+        case ScriptActionType::world_query_terrain: return ScriptWorldActionType::query_terrain;
+        case ScriptActionType::world_query_nearby: return ScriptWorldActionType::query_nearby;
         default: return std::nullopt;
     }
 }
