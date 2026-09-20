36a747d1205b9a88b5851e1d1abe2b027e95f319
OpenGenesis (MPL 2.0) <153564363+opengenesislink@users.noreply.github.com>
2026-09-20 19:34:46 +0200

    Expand native physics world contract for 12.0

--- include/opengenesis/physics/physics_world.hpp
+++ include/opengenesis/physics/physics_world.hpp
@@ -4,6 +4,7 @@
 #include <functional>
 #include <mutex>
 #include <unordered_map>
+#include <vector>
 
 namespace opengenesis::physics {
 
@@ -21,8 +22,34 @@ struct Body {
     std::uint64_t id{0};
     Vec3 position{}, velocity{};
     Vec3 rotation{}, angular_velocity{};
-    double mass{1.0}, restitution{0.15}, radius{0.5};
+    Vec3 force{}, torque{};
+    double mass{1.0};
+    double restitution{0.15};
+    double friction{0.6};
+    double radius{0.5};
+    double linear_damping{0.04};
+    double angular_damping{0.04};
+    double gravity_scale{1.0};
+    double buoyancy{0.0};
     bool dynamic{true};
+    bool character{false};
+};
+
+struct CollisionContact {
+    std::uint64_t body_a{0};
+    std::uint64_t body_b{0};
+    Vec3 point{};
+    Vec3 normal{};
+    double impulse{0.0};
+    bool ground{false};
+};
+
+struct DistanceConstraint {
+    std::uint64_t id{0};
+    std::uint64_t body_a{0};
+    std::uint64_t body_b{0};
+    double rest_length{0.0};
+    double stiffness{1.0};
 };
 
 class PhysicsWorld final {
@@ -33,10 +60,29 @@ public:
     bool set_body_velocity(std::uint64_t id, Vec3 velocity);
     bool set_body_rotation(std::uint64_t id, Vec3 rotation);
     bool set_body_angular_velocity(std::uint64_t id, Vec3 angular_velocity);
+    bool set_body_material(std::uint64_t id, double mass,
+                           double restitution, double friction);
+    bool set_body_damping(std::uint64_t id, double linear_damping,
+                          double angular_damping);
+    bool set_body_gravity_scale(std::uint64_t id, double gravity_scale);
+    bool set_body_buoyancy(std::uint64_t id, double buoyancy);
+    bool set_body_radius(std::uint64_t id, double radius);
+    bool apply_force(std::uint64_t id, Vec3 force);
+    bool apply_impulse(std::uint64_t id, Vec3 impulse);
+    bool apply_torque(std::uint64_t id, Vec3 torque);
+
+    std::uint64_t add_distance_constraint(std::uint64_t body_a,
+                                          std::uint64_t body_b,
+                                          double rest_length,
+                                          double stiffness = 1.0);
+    bool remove_constraint(std::uint64_t constraint_id);
+
     void step(double dt);
 
     [[nodiscard]] std::size_t body_count() const;
     [[nodiscard]] Body body(std::uint64_t id) const;
+    [[nodiscard]] std::vector<CollisionContact> collisions() const;
+    [[nodiscard]] std::vector<DistanceConstraint> constraints() const;
 
     void set_gravity(Vec3 gravity);
     void set_ground_height(double height);
@@ -45,7 +91,10 @@ public:
 private:
     mutable std::mutex mutex_;
     std::unordered_map<std::uint64_t, Body> bodies_;
+    std::unordered_map<std::uint64_t, DistanceConstraint> constraints_;
+    std::vector<CollisionContact> collisions_;
     std::uint64_t next_id_{1};
+    std::uint64_t next_constraint_id_{1};
     Vec3 gravity_{0, 0, -9.81};
     double ground_height_{0.0};
     std::function<double(double, double)> ground_sampler_;
